644ccab86491aea13d880c5c2c8ebb386ae1e697
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-12-24 09:03:47 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -1,8 +1,8 @@
 class BaseClass{
-    constructor(x, y, width, height, angle) {
+    constructor(x, y, width, height) {
         var options = {
             'restitution':0.4,
-            // 'friction':1.0,
+            'friction':1.0,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
@@ -12,12 +12,13 @@ class BaseClass{
       }
       display(){
         var angle = this.body.angle;
+        var pos = this.body.position;
         push();
-        translate(this.body.position.x, this.body.position.y);
+        translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
-      fill(8, 225, 215);
-      rect(pos.x, pos.y, this.width, this.height);
+        fill(8, 225, 215);
+        rect(0, 0, this.width, this.height);
         pop();
       }
 }

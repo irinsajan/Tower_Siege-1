f527cea044b32f7762695279138a0c142af0211f
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-12-24 09:05:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var floor, platform;
-var block1, block2, block3, block4, block5, block6, block7, block8, block9, block10, block11, block12, block13, block14, block15;
+var ground, platform;
+var block1, block2, block3, block4, block5, block6, block7, block8, block9, block10, block11, block12, block13, block14, block15, block16;
 
 function setup() {
   createCanvas(800,800);
@@ -12,26 +12,27 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  floor = new Ground(400, 700, 900, 5);
+  ground = new Ground(400, 700, 900, 5);
 
   platform = new Ground(400, 650, 300, 5);
 
   fill(11, 247, 237);
-  block1 = new Block(360, 620, 20, 30);
-  block2 = new Block(380, 620, 20, 30);
-  block3 = new Block(400, 620, 20, 30);
-  block4 = new Block(420, 620, 20, 30);
-  block5 = new Block(440, 620, 20, 30); 
-  block6 = new Block(370, 590, 20, 30);
-  block7 = new Block(390, 590, 20, 30);
-  block8 = new Block(410, 590, 20, 30);
-  block9 = new Block(430, 590, 20, 30);
-  block10 = new Block(380, 560, 20, 30);
-  block11 = new Block(400, 560, 20, 30);
-  block12 = new Block(420, 560, 20, 30);
-  block13 = new Block(390, 530, 20, 30);
-  block14 = new Block(410, 530, 20, 30);
-  block15 = new Block(400, 500, 20, 30);
+  block1 = new Block(340, 620, 20, 30);
+  block2 = new Block(360, 620, 20, 30);
+  block3 = new Block(380, 620, 20, 30);
+  block4 = new Block(400, 620, 20, 30);
+  block5 = new Block(420, 620, 20, 30); 
+  block6 = new Block(440, 620, 20, 30);
+  block7 = new Block(460, 620, 20, 30);
+  block8 = new Block(360, 590, 20, 30);
+  block9 = new Block(380, 590, 20, 30);
+  block10 = new Block(400, 590, 20, 30);
+  block11 = new Block(420, 590, 20, 30);
+  block12 = new Block(440, 590, 20, 30);
+  block13 = new Block(400, 560, 20, 30);
+  block14 = new Block(380, 560, 20, 30);
+  block15 = new Block(420, 560, 20, 30);
+  block16 = new Block(400, 530, 20, 30);
 
 }
 
@@ -55,10 +56,11 @@ function draw() {
   block13.display();
   block14.display();
   block15.display();
+  block16.display();
 
   platform.display();
  
-  floor.display();
+  ground.display();
 
- drawSprites();
+ 
 }
